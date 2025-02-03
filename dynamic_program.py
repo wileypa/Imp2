4d0063ac980beac9e5fd487eb83de19a4bc3d026
Paige Wiley <wileypa@oregonstate.edu>
2025-02-02 16:05:26 -0800

    possibly finished the algorithm. Needs to be checked

--- dynamic_program.py
+++ dynamic_program.py
@@ -48,7 +48,7 @@ def initialize_matrices(seq1, seq2, loss_matrix, x_indexdict, y_indexdict):
     for i in range(1, m+1):
         row = x_indexdict[seq1[i-1]]
         D[i][0] = D[i - 1][0] + loss_matrix[row][-1] 
-        ptr[i][0] = 'D'  
+        ptr[i][0] = 'U'  
     for j in range(1, n+1):
         column = y_indexdict[seq2[j-1]]
         D[0][j] = D[0][j - 1] + loss_matrix[-1][column]
@@ -84,24 +84,23 @@ def edit_dist(seq1, seq2, loss_matrix, x_indexdict, y_indexdict): #m is length o
 
     for i in range(1, m+1):
         for j in range(1, n+1):
-            D[i][j] = min(
-                        D[i-1][j] + 1,
-                        D[i][j-1] + 1,
-                        D[i-1][j-1] + diff(seq1[i - 1], seq2[j - 1]))
-            if D[i][j] == D[i-1][j] + 1:
-                direction = 'U' #up
-            elif D[i][j] == D[i][j-1] + 1:
-                direction = 'L' #left 
-            elif D[i][j] == D[i-1][j-1]:
-                direciton = 'D' #diag
+            substitution = D[i-1][j-1] + diff(seq1[i - 1], seq2[j - 1])
+            insertion = D[i][j-1] + 1
+            deletion = D[i-1][j] + 1
 
-            #assign the ptr based on which direction is is
-            if direction == 'U': #point to the one to the up
-                ptr[i][j] = direction 
-            elif direction == 'L': #point to the one to the left
-                ptr[i][j] = direction
-            elif direction == 'D': #point to the one diagonally
-                ptr[i][j] = 'D' 
+            D[i][j] = min(insertion, deletion, substitution)
+            print("dij; ", D[i][j])
+            print("delet ", deletion)
+            print("inserty ", insertion)
+            print("sub ", substitution)
+            if D[i][j] == deletion:
+                direction = 'U' #up
+            elif D[i][j] == insertion:
+                direction = 'L' #left 
+            elif D[i][j] == substitution:
+                direction = 'D' #diag
+            print("direction: ", direction) 
+            ptr[i][j] = direction
     
     print_matrix(D, "D Matrix")
     print_matrix(ptr, "Pointer Matrix")
@@ -120,13 +119,27 @@ def backtracing(ptr, seq1, seq2):
     #i = 4
     #j = 4
 
-    # while i > 0 or j > 0:
-    #     #want to map out the pointer
-    #     print("here")
-
-
+    while i > 0 or j > 0:
+        if ptr[i][j] is None:
+            break
+        if ptr[i][j] == 'D': #substitution
+            aligned_seq1.append(seq1[i-1])
+            aligned_seq2.append(seq2[j-1])
+            i -= 1
+            j -= 1
+        elif ptr[i][j] == 'U': #deletion
+            aligned_seq1.append(seq1[i-1])
+            aligned_seq2.append('-')
+            i -= 1
+        elif ptr[i][j] == 'L': #insertion
+            aligned_seq1.append('-')
+            aligned_seq2.append(seq2[j-1])
+            j -= 1
+    return list(reversed(aligned_seq1)), list(reversed(aligned_seq2))
 
 matrix_info = read_cost_matrix("imp2cost.txt")
 sequences = read_sequences("imp2input.txt")
 D, ptr = edit_dist(sequences[0], sequences[1], matrix_info[0], matrix_info[1], matrix_info[2])
 aligned1, aligned2 = backtracing(ptr, sequences[0], sequences[1])
+print("Sequence One ", aligned1)
+print("Sequence Two ", aligned2)
